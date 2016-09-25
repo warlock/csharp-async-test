464eb42e1714f7e10802f122b2d71be3e6d72008
Josep Subils Rigau <josep@spellbook.io>
2016-09-25 21:36:17 +0200

    First commit

---
+++ CshAsyncTest/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CshAsyncTest
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("async testing!");
+			AsynMeths a = new AsynMeths();
+			a.Bu();
+			System.Threading.Thread.Sleep(20000);
+		}
+	}
+
+	class AsynMeths
+	{
+		public static Task<string> Make()
+		{
+			return Task.Run(() =>
+			{
+				Console.WriteLine("llençat " + DateTime.Now.ToString());
+				System.Threading.Thread.Sleep(5000);
+				return "hola " + DateTime.Now.ToString();
+			});
+		}
+
+		public static void Make2()
+		{
+			Task.Run(() =>
+			{
+				Console.WriteLine("llençat " + DateTime.Now.ToString());
+				System.Threading.Thread.Sleep(5000);
+				Console.WriteLine("hola " + DateTime.Now.ToString());
+			});
+		}
+
+		public async virtual void Boh()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine("llençant" + i);
+				string text = await Make();
+				Console.WriteLine(text);
+			}
+			Console.WriteLine("he acabat d'iterar");
+		}
+
+
+		public virtual void Bu()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine("llençant" + i);
+				Make2();
+			}
+			Console.WriteLine("he acabat d'iterar");
+		}
+	}
+}
